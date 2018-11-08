c26a016f09f9f612bebd924b3462ffbd49e7c765
CarsonRey <c.rey494@gmail.com>
2018-11-08 13:26:13 -0500

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,54 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    set_employee
+  end
+
+  def new
+    @employee = Employee.new
+    @dogs = Dog.all
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    @dogs = Dog.all
+    if @employee.save
+      redirect_to @employee
+    else
+      render :new
+    end
+  end
+
+  def edit
+    set_employee
+    @dogs = Dog.all
+  end
+
+  def update
+    set_employee
+    if @employee.update(employee_params)
+      redirect_to @employee
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    set_employee.destroy
+    redirect_to employees_path
+  end
+
+  private
+
+  def set_employee
+    @employee = Employee.find(params[:id])
+  end
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+  end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,13 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :alias, uniqueness: true
+  validates :title, uniqueness: true
+  validates :dog_id, presence: true
+
+
+  def name
+    self.first_name + " " +  self.last_name
+  end
 end
